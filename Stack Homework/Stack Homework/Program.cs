5b23458881be6a53ed03e077653c329ab31db3a4
karimh93 <karim.husein0293@gmail.com>
2019-07-23 18:16:04 +0300

    First Commit

---
+++ Stack Homework/Stack Homework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+
+           
+            Console.WriteLine($"The first element is:{stack.Peek()}");
+
+            
+
+            var laststack = stack.Last();
+
+            Console.WriteLine($"The last element is:{ laststack}");
+
+            
+            
+
+
+        }
+    }
+}
